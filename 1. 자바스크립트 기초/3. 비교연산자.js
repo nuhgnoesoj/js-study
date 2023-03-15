2fdbabb6381d2ca32ee6117872a482c33f67a88f
nuhgnoesoj <126743224+nuhgnoesoj@users.noreply.github.com>
2023-03-15 20:54:21 +0900

    0315(수)

---
+++ 1. 자바스크립트 기초/3. 비교연산자.js
@@ -0,0 +1,29 @@
+
+var a = 5;
+var b = '5';
+
+console.log(a === b);
+console.log(a !== b);
+
+console.log('====================');
+
+//  == 비교는 결과 예측이 어려움
+console.log('0' === '');
+console.log(0 === '');
+console.log('0' === 0);
+console.log(false === 'false');
+console.log(false === '0');
+
+//  문자 대소비교
+console.log('====================');
+console.log('cobra' < 'cobrO');
+//  대문자 A:65 소문자 a:97
+
+
+var money = 0;
+
+if (!money) {
+    console.log('저는 그지입니다.');
+} else {
+    console.log('저는 돈이 있습니다.');
+}
